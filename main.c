98084bfd603f92fd6e21a9600d430abef0deb36a
Your Name <you@example.com>
2019-01-16 10:14:21 +0800

    kungjia

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main(){
-
-    printf("hello world!");
+     int code = 0;
+    printf("guanliminganzifuxitong\n");
+    while (1){
+        printf("1----tianjia\n");
+        printf("2----shanchuzuihouyigeminganzifu\n");
+        printf("3----chakansuoyouminganzifu\n");
+        printf("4----tihuan\n");
+        printf("5----tuichu\n");
+        printf("xuanzexiangyinggongneng\n");
+        scanf("%d",&code);
+    }
 
     return 0;
 
